2a9515e793bee35a288d183b3d54eb992dcfd5a5
George Karaviotis <geokaraviotis@gmail.com>
2024-10-30 19:16:42 +0200

    Configured the file upload api further

--- fhapp/api/serializers.py
+++ fhapp/api/serializers.py
@@ -9,4 +9,9 @@ class BotModelSerializer(serializers.ModelSerializer):
 class FileUploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataSource
-        fields = ['file']
+        fields = ['file', 'botId']
+
+    def create(self, validated_data):
+        bot_id = validated_data.pop('bot_id')
+        bot = Bot.objects.get(id=bot_id)  # Retrieve the Bot instance
+        return DataSource.objects.create(bot=bot, **validated_data)
