2a7fb2847ba6d37dd8821c9951cb3025a56d4031
George Karaviotis <geokaraviotis@gmail.com>
2024-10-30 19:09:22 +0200

    Migrated to new auth user model and registered all models in the admin panel

--- fhapp/admin.py
+++ fhapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Bot
+from .models import *
 
 # Register your models here.
 admin.site.register(Bot)
+admin.site.register(User)
+admin.site.register(DataSource)
+admin.site.register(QuestionHistory)
