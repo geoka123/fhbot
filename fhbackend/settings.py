bfc61a8f8237eba82d71c7881da7959d8133ab10
George Karaviotis <geokaraviotis@gmail.com>
2024-10-30 15:10:54 +0200

    Added file url and dield to the bot model

--- fhbackend/settings.py
+++ fhbackend/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# MEDIA_URL = "documents/"
+MEDIA_URL = "documents/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
