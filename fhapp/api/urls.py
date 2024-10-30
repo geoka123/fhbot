d44cb685dbbb0d1458f25de53c0a2636560411f2
George Karaviotis <geokaraviotis@gmail.com>
2024-10-30 14:18:46 +0200

    Added a view to see all created bots

--- fhapp/api/urls.py
+++ fhapp/api/urls.py
@@ -2,5 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('hello/', IndexExampleView.index_hello)
+    path('hello/', IndexExampleView.index_hello),
+    path('botsnow/', AllBotsView.get_all_bots),
 ]
