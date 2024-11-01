62dcd07f0c334016302d7aafa6f5c5cc995d9e6e
George Karaviotis <geokaraviotis@gmail.com>
2024-11-01 15:15:22 +0200

    Increased max length

--- fhapp/__init__.py
+++ fhapp/__init__.py
@@ -1,1 +1,1 @@
-default_app_config = 'fhapp.apps.MyAppConfig'
+# default_app_config = 'fhapp.apps.MyAppConfig'
--- fhapp/apps.py
+++ fhapp/apps.py
@@ -4,12 +4,12 @@ class FhappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fhapp'
 
-class MyAppConfig(AppConfig):
-    name = 'fhapp'
+# class MyAppConfig(AppConfig):
+#     name = 'fhapp'
 
-    def ready(self):
-        # Import and call your custom functions here
-        from .llm_rag_config import lllm_init, index_init, query_engine_init
-        lllm_init()
-        index_init()
-        query_engine_init()
+#     def ready(self):
+#         # Import and call your custom functions here
+#         from .llm_rag_config import lllm_init, index_init, query_engine_init
+#         lllm_init()
+#         index_init()
+#         query_engine_init()
